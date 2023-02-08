7ad73e775859c26eb51ff596fb3600d1e38cd3a9
Ahmad Hakroosh <ahmad@letsrework.com>
2023-02-08 17:02:19 +0200

    New release

--- @common/src/events/index.ts
+++ @common/src/events/index.ts
@@ -1,3 +1,4 @@
 export * from "./base";
 export * from "./interfaces";
 export * from "./types";
+export * from "./enums";
--- @common/src/events/types/index.ts
+++ @common/src/events/types/index.ts
@@ -1,3 +1,2 @@
 export * from "./event-data";
 export * from "./subject";
-export * from "./order-status";
